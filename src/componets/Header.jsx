c4dd563fca3f31e9161ab1237bcf1c1adb324446
thinkwithabs <twithabs@gmail.com>
2022-09-03 17:24:20 +0600

    adddd

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import logo from "../componets/img/logo.png";
 import { FaShoppingBasket } from "react-icons/fa";
 import avater from "../componets/img/avatar.png";
@@ -7,22 +7,28 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebaseConfig";
 import { useStateValue } from "../contex/stateProvider";
 import { actionType } from "../contex/reducer";
+import { GrAdd,GrLogout } from "react-icons/gr";
 
 const Header = () => {
-    const [{user},dispatch]= useStateValue()
-    const firebaseAuth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+  const [{ user }, dispatch] = useStateValue();
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
-    
+  const [isMount,setMount] = useState(false)
+
   const login = async () => {
-       if(!user){
-        const {user:{refreshToken,providerData}} = await signInWithPopup(firebaseAuth,provider) 
-        dispatch({
-            type:actionType.SET_USER,
-            user:providerData[0]
-        });
-        localStorage.setItem("user",JSON.stringify(providerData[0]))
-       }
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }else{
+      setMount(!isMount)
+    }
   };
 
   return (
@@ -48,15 +54,25 @@ const Header = () => {
           </p>
         </div>
 
-        <div>
+        <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
             className="w-8 min-w-[40px] h-8 min-h-[40px] cursor-pointer ml-5 drop-shadow-xl rounded-full"
-            src={user? user.photoURL :
-                avater}
+            src={user ? user.photoURL : avater}
             alt=""
             onClick={login}
           />
+
+          {isMount && <div className="w-40 absolute bg-gray-50 top-12 right-0">
+            <p className=" px-4 py-2 cursor-pointer flex items-center font-normal hover:bg-slate-100 transition-all duration-100 ease-in-out text-base">
+              Creact Item
+              <GrAdd className="pl-1"/>
+            </p>
+            <p className=" px-4 py-2 cursor-pointer flex items-center font-normal hover:bg-slate-100 transition-all duration-100 ease-in-out text-base">
+              Log Out
+              < GrLogout className="pl-1"/>
+            </p>
+          </div>}
         </div>
       </div>
 
